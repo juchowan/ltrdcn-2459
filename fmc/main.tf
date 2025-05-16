afeb401a627dbc538f4d044644f66743014db860
Justyna Chowaniec <juchowan@cisco.com>
2025-05-16 08:07:20 +0200

    update modules

--- fmc/main.tf
+++ fmc/main.tf
@@ -2,22 +2,19 @@ terraform {
   required_providers {
     fmc = {
       source  = "CiscoDevNet/fmc"
-      version = "2.0.0-beta1"
+      version = "2.0.0-beta2"
     }
   }
 }
 
 module "fmc" {
   source            = "netascode/nac-fmc/fmc"
-  version           = "0.0.1"
+  version           = "0.0.2"
   
   yaml_directories = ["data"]
 
   manage_deployment = var.manage_deployment
 }
 
-#provider "fmc" {
-#    username  = "apiadmin"
-#    password  = "C1sco12345"
-#    url       = "https://198.18.130.200"
-#}
+provider "fmc" {
+}
